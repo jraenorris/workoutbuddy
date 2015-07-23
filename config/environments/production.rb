0e7ba1285a9aef1b7a571a0bc2a93d5a8fc5d6a5
Jesse Norris <jesserae.norris@gmail.com>
2015-07-23 14:22:15 -0400

    testing email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,6 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.delivery_method = :smtp
 end
 
 ActionMailer::Base.smtp_settings = {
